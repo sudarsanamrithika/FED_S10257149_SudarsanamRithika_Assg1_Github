8b6a6f56370067410882809b987ea7013bc78dcd
sudarsanamrithika <veethe2nd@gmail.com>
2023-11-27 11:11:15 +0800

    complete w/ no content + halfway responsive YIPPEE

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Get the active tab from the URL query parameters
-  var urlParams = new URLSearchParams(window.location.search);
-  var activeTab = urlParams.get('tab');
-  if (activeTab) {
-    changeTab(activeTab);
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-});
-
-function changeTab(tabId) {
-  var tabs = document.querySelectorAll('.tab');
-  tabs.forEach(function(tab) {
-    tab.classList.remove('active');
-  });
-
-  var clickedTab = document.getElementById(tabId);
-  clickedTab.classList.add('active');
-
-  // Update the URL with the active tab
-  history.replaceState({}, '', window.location.pathname + '?tab=' + tabId);
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
